1b1932db647cb0b0ad524b51d9940f8b56feb8b7
Bjorn Sandvik <bjorn.sandvik@gmail.com>
2016-12-04 12:42:34 +0100

    Drawer handling

--- app/js/MarkerControl.js
+++ app/js/MarkerControl.js
@@ -16,6 +16,7 @@ export const MarkerControl = L.Control.extend({
 
     _initLayout() {
         const container = this._container = L.DomUtil.create('div', 'leaflet-control-marker');
+        const dialog = this._dialog = document.querySelector('dialog');
 
         L.DomEvent.disableClickPropagation(container);
         if (!L.Browser.touch) {
@@ -25,20 +26,35 @@ export const MarkerControl = L.Control.extend({
         container.title = 'Add marker to map';
         L.DomUtil.create('span', 'material-icons', container).innerText = 'add_location';
 
-        L.DomEvent.on(container, 'click', this._onClick, this);
-
-        this._dialog = document.querySelector('dialog');
+        L.DomEvent.on(container, 'click', () => dialog.showModal());
 
         // Add polyfill if needed
-        if (!this._dialog.showModal) {
-            dialogPolyfill.registerDialog(this._dialog);
+        if (!dialog.showModal) {
+            dialogPolyfill.registerDialog(dialog);
         }
 
-        this._dialog.querySelector('.close').addEventListener('click', () => this._dialog.close());
+        dialog.querySelector('.close').addEventListener('click', () => dialog.close());
+        dialog.querySelector('.ok').addEventListener('click', this.addMarker.bind(this));
     },
 
-    _onClick() {
-        this._dialog.showModal();
+    addMarker() {
+        const dialog = this._dialog;
+        const longitude = dialog.querySelector('#longitude').value;
+        const latitude = dialog.querySelector('#latitude').value;
+
+        dialog.close();
+
+        // Only allow one marker
+        if (this._marker) {
+            this._map.removeLayer(this._marker);
+        }
+
+        this._marker = L.marker([latitude, longitude]).addTo(this._map);
+
+
+
+        // window.history.pushState({}, null, `?longitude=${longitude}&latitude=${latitude}`);
+
     }
 });
 
